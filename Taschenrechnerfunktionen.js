816143ec2de9f9daaf7ef619f8cf4b24157585e7
StefanieSevens <stefaniesevens@aol.com>
2023-02-23 02:16:12 +0100

    Next Commit

---
+++ Taschenrechnerfunktionen.js
@@ -0,0 +1,34 @@
+//Addition:
+
+function addition (x,y) {
+    return x + y
+}
+
+
+//Subtraktion:
+
+function subtraction (x,y) {
+    return x - y
+}
+
+//Division:
+
+function division (x,y) {
+    return x/y
+}
+
+//Multiplikation:
+
+function multiplication (x,y) {
+    return x * y 
+}
+
+console.log(addition (x,y))
+console.log(substraction(x,y))
+console.log(division(x,y))
+console.log(multiplication(x * y))
+
+
+
+
+
